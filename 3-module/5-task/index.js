80dbab043341624909fc16b3371a67190d4ee562
Anton Ivanichenko <anton.ivanichenko@gmail.com>
2023-09-24 19:59:56 +0530

    accomplish 5-task get min/max

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,11 @@
 function getMinMax(str) {
-  // ваш код...
+  let numArray = str.split(' ')
+    .reduce((array, item) => {
+
+      if (+item) array.push(+item);
+      return array;
+    }, [])
+    .sort((a, b) => a - b);
+
+  return { min: numArray.at(0), max: numArray.at(-1) };
 }
