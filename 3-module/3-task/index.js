d89d58503a754160645fab6c4a6c07284843e530
Anton Ivanichenko <anton.ivanichenko@gmail.com>
2023-09-24 16:08:36 +0530

    accomplish 3-task camelize

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,10 @@
 function camelize(str) {
-  // ваш код...
+
+  return str.split('-')
+    .map((word, index) => {
+
+      if (index > 0) return word[0].toUpperCase() + word.slice(1);
+      return word;
+    })
+    .join('');
 }
