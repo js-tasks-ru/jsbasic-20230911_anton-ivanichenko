48745a2c17ad19ea8d5e2156300dc1c1b0b63e83
Anton Ivanichenko <anton.ivanichenko@gmail.com>
2023-10-12 16:40:44 +0530

    accomplish 6-module 1-task users table

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,50 @@
  *
  */
 export default class UserTable {
+  elem = null;
+
   constructor(rows) {
+    this.rows = rows;
+
+    this.#render();
+  }
+
+  #render() {
+    const container = document.createElement('div');
+    container.innerHTML = this.#template();
+
+    this.elem = container;
+
+    this.elem.addEventListener('click', this.#onButtonClick);
+  }
+
+  #onButtonClick(event) {
+
+    if (event.target.nodeName == 'BUTTON') event.target.closest('tr').remove();
+  }
+
+  #template() {
+
+    return `<table>
+    <thead>
+        <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>${this.rows.map((user) => {
+      return `<tr>
+            <td>${user.name}</td>
+            <td>${user.age}</td>
+            <td>${user.salary}</td>
+            <td>${user.city}</td>
+            <td><button>X</button></td>
+        </tr>`
+    })}
+        
+        </table>`;
   }
 }
